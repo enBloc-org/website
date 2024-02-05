401df6a718ff27c667e65aab42653eb1fbd3e25a
camelPhonso <114600712+camelPhonso@users.noreply.github.com>
2024-02-05 16:52:46 +0000

    added copy to the 'our work' section

--- src/components/OurWorkSection.tsx
+++ src/components/OurWorkSection.tsx
@@ -9,12 +9,43 @@ const OurWorkSection: React.FC<OurWorkProps> = ({ workRef }) => {
   return (
     <section ref={workRef}>
       <h2
-        className='bg-en-blue pl-small-screen xl:pl-large-screen mr-20 mt-stack-lg 
-      max-w-[1000px] rounded-r-full p-8 text-4xl text-white'
+        className='mb-stack-md mr-20 mt-stack-lg max-w-[1000px] rounded-r-full bg-en-blue 
+      p-8 pl-small-screen text-4xl text-white xl:pl-large-screen'
       >
-        Our Work
+        Recent Work
       </h2>
-      <p className='m-20 text-xl italic'>Section coming soon</p>
+      <div className='grid grid-cols-2 grid-rows-2'>
+        <div className='m-5 w-3/4 rounded-lg bg-en-orange p-8 text-white'>
+          <h3 className='pb-4 text-2xl italic'>Cambridge University</h3>
+          <p>
+            In collaboration with researchers at Cambridge University, our team
+            at enBloc had the opportunity to contribute to academic discovery.
+            <br />
+            <br /> In this project we built a bespoke scraper capable of
+            extracting insights from over 8 million archival files.
+          </p>
+        </div>
+        <div className='m-5 w-3/4 rounded-lg bg-en-orange p-8 text-white'>
+          <h3 className='pb-4 text-2xl italic'>Kindly</h3>
+          <p>
+            Trafalgar Girls, a charity supporting Ukrainian refugees in the UK,
+            needed a dedicated marketplace to allow for the streamlined
+            management of donations within their community.
+            <br />
+            <br /> In response, the Kindly app allows donors and refugees to
+            communicate, provide and receive support as needed.
+          </p>
+        </div>
+        <div className='m-5 w-3/4 rounded-lg bg-en-orange p-8 text-white'>
+          <h3 className='pb-4 text-2xl italic'>Cinesphere</h3>
+          <p>
+            Discovering independent cinema in London is made easier with
+            Cinesphere. <br />
+            <br /> This tailor-made tool for cinephiles offers information on
+            showings at various independent movie theatres.
+          </p>
+        </div>
+      </div>
     </section>
   );
 };
